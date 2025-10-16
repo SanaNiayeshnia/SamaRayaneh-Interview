5208c10521522a4ce11a0ea611df8a9ddf4210d1
Sana Niayeshnia <niayeshnia1381@gmail.com>
2025-10-16 16:48:00 +0330

    add loading pages

---
+++ app/_components/ui/Loader.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+function Loader() {
+  return (
+    <Image src="/images/loader.svg" alt="loader" width="100" height="100" />
+  );
+}
+
+export default Loader;
---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import Loader from "./_components/ui/Loader";
+
+function Loading() {
+  return (
+    <div className="grid place-content-center place-items-center h-full">
+      <Loader />
+    </div>
+  );
+}
+
+export default Loading;
