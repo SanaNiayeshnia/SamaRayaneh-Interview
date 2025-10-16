cf3a9077186ef419bc10d4cc72df58b44a944e9b
Sana Niayeshnia <niayeshnia1381@gmail.com>
2025-10-16 17:38:51 +0330

    handle redirecting authenticated users who try to access the login page back to the dashboard page in middleware

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
 import Image from "next/image";
 import LoginForm from "./_components/login/LoginForm";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  //redirect the user to the dashboard if they are authenticated
-  const isUserAuthenticated = (await cookies())?.get("accessToken")?.value;
-  if (isUserAuthenticated) redirect("/dashboard");
-
   return (
     <div className="display grid  place-content-center h-screen ">
       <div className="flex items-center gap-5 flex-col border-3 border-primary-500 py-5 px-10 rounded-md ">
--- middleware.js
+++ middleware.js
@@ -2,17 +2,25 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const accessToken = request.cookies.get("accessToken")?.value;
+  const url = request.nextUrl;
+  const isOnLoginPage = url.pathname === "/";
 
-  if (!accessToken) {
-    //Redirect to the login page when the user is not authenticated
+  if (!accessToken && !isOnLoginPage) {
+    // Redirect unauthenticated users to login page
     const loginUrl = new URL("/", request.url);
     return NextResponse.redirect(loginUrl);
   }
 
-  //Otherwise, continue
+  if (accessToken && isOnLoginPage) {
+    // Redirect authenticated users who try to access the login page back to the dashboard page
+    const dashboardUrl = new URL("/dashboard", request.url);
+    return NextResponse.redirect(dashboardUrl);
+  }
+
+  // Otherwise, continue
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/", "/dashboard/:path*"],
 };
