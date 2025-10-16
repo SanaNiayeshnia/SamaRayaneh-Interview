59413d10af9b60e0a5d751146ff2ee740d2d6147
Sana Niayeshnia <niayeshnia1381@gmail.com>
2025-10-16 13:52:55 +0330

    configure the request interceptors

--- app/_lib/request.js
+++ app/_lib/request.js
@@ -1,5 +1,22 @@
+import { cookies } from "next/headers";
+
 const { default: axios } = require("axios");
 
 export const request = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
 });
+
+request.interceptors.request.use(
+  (config) => {
+    // Get token from cookies (or localStorage if not HttpOnly)
+    const token = cookies().get("accessToken")?.value;
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
